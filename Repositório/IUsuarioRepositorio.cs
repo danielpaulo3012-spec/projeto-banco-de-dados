a8131d21adcf2d4b38198fc8f31beca1aa2ece52
DANIEL PAULO MASCARENHAS MORBIDA DA SILVA <00001117545891sp@aluno.educacao.sp.gov.br>
2026-04-24 14:34:28 -0300

    adicionando projeto

---
+++ Repositório/IUsuarioRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using Segundo_App_BancoDados.Models;
+
+namespace Segundo_App_BancoDados.Repositório.Contract
+{
+    public interface IUsuarioRepositorio
+    {
+        // GRUD - Create, Read, Update, Delete
+
+        IEnumerable<Usuario> ObterTodosUsuarios();
+        void Cadastrar(Usuario usuario);
+        void Atualizar(Usuario usuario);
+        Usuario ObterUsuario(int id);
+        void Excluir(int id);
+        
+
+    }
+}
