5468d92961709cf0b71bbb1e125e1640610aad02
DANIEL PAULO MASCARENHAS MORBIDA DA SILVA <00001117545891sp@aluno.educacao.sp.gov.br>
2026-04-24 15:27:52 -0300

    adicionando controlador

--- wwwroot/Banco/script usuario.sql
+++ wwwroot/Banco/script usuario.sql
@@ -7,3 +7,7 @@ nomeUsu varchar(50) not null,
 cargo varchar (30) not null,
 dataNasc datetime 
 );
+insert into usuario(nomeUsu, cargo, dataNasc)
+            values("Yan", "Admin", "1978/05/01"),
+                  ("Murilo","Colaborador","2008/05/01");
+select * from usuario;
